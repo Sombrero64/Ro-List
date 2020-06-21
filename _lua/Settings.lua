6e3bce72c245cb83c9a3f63f0249bafb24c98f79
Samuel <67114976+samthatwoz@users.noreply.github.com>
2020-06-20 22:18:14 -0500

    Create Settings.lua

---
+++ _lua/Settings.lua
@@ -0,0 +1,14 @@
+local Settings = 
+{
+	-- Only allow whitelisted users from accessing your place.
+	["Only Whitelisted"] = false,
+	-- A table/arrey/list of users that are whitelisted.
+	["Whitelisted Users"] = {""},
+	-- The kick message if the player isn't whitelisted.
+	["Whitelist Kick"] = "",
+	-- A table/arrey/list of users that are blacklisted.
+	["Blacklisted Users"] = {""},
+	-- The kick message if the player is blacklisted.
+	["Blacklist Kick"] = ""
+}
+return Settings
